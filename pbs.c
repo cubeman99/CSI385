7c854949445d42c4c2ddb5aacb42ead26c10951f
JoeyGally <joseph.gallahan@mymail.champlain.edu>
2016-10-13 18:16:01 -0400

    Add files via upload
    Shell and pbs updates

---
+++ pbs.c
@@ -0,0 +1,54 @@
+#include "fat.h"
+#include <string.h>
+
+void printBootSector();
+
+int main()
+{
+
+   // Open the disk image file.
+   FILE_SYSTEM_ID = fopen("../disks/floppy1", "r+");
+   if (FILE_SYSTEM_ID == NULL)
+   {
+      printf("Could not open the floppy drive or image.\n");
+      return 1;
+   }
+
+   // Load the boot sector.
+   if (loadFAT12BootSector() != 0)
+   {
+      printf("Something has gone wrong -- could not read the boot table\n");
+      return 1;
+   }
+  // stuff = (unsigned char*) malloc(BYTES_PER_SECTOR);
+   printBootSector();
+
+   return 0;
+}
+
+void printBootSector()
+{   
+   char volumeLabel[12];
+   strcpy(volumeLabel, FAT_BOOT_SECTOR.volumeLabel);
+   volumeLabel[11] = '\0';
+   
+   char fileSystemType[9];
+   strcpy(fileSystemType, FAT_BOOT_SECTOR.fileSystemType);
+   fileSystemType[8] = '\0';
+   
+   printf("Bytes Per Sector: %d\n", FAT_BOOT_SECTOR.bytesPerSector);
+   printf("Sectors Per Cluster: %d\n", FAT_BOOT_SECTOR.sectorsPerCluster);
+   printf("Number of FATs: %d\n", FAT_BOOT_SECTOR.numFATs);
+   printf("Number of Reserved Sectors: %d\n", FAT_BOOT_SECTOR.numReservedSectors);
+   printf("Number of Root Entries: %d\n", FAT_BOOT_SECTOR.maxNumRootDirEntries);
+   printf("Total Sector Count: %d\n", FAT_BOOT_SECTOR.totalSectorCount);
+   printf("Sectors Per Fat: %d\n", FAT_BOOT_SECTOR.sectorsPerFAT);
+   printf("Sectors Per Track: %d\n", FAT_BOOT_SECTOR.sectorsPerTrack);
+   printf("Number of Heads: %d\n", FAT_BOOT_SECTOR.numHeads);
+   printf("Boot Signature (hex): 0x%02x\n", FAT_BOOT_SECTOR.bootSignature);
+   printf("Volume ID (hex): 0x%06x\n", FAT_BOOT_SECTOR.volumeID);
+   printf("Volume Label: %s\n", volumeLabel);
+   printf("File System Type: %s\n", fileSystemType);
+}
+
+
---
+++ shell.c
@@ -0,0 +1,90 @@
+/*****************************************************************************
+ * Author: David Jordan & Joey Gallahan
+ * 
+ * Description: Shell V0.1 
+ *
+ * Certification of Authenticity:
+ * I certify that this assignment is entirely my own work.
+ ****************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define FALSE 0
+#define TRUE 1
+
+void displayPrompt();
+void readCommand(char* command, char** params);
+
+int main()
+{
+   char command[32];
+   char* params[32];
+   int status;
+   int exitShell = FALSE;
+
+   while (exitShell != TRUE)
+   {
+      displayPrompt();
+      readCommand(command, params);
+      
+      if (strcmp(command, "exit") == 0)
+      { 
+         exitShell = TRUE;
+      }
+      else
+      {
+         if(fork() != 0)
+         {
+            waitpid(-1, &status, 0);
+         }
+         else
+         {
+            execve(command, params, 0);
+      	 }
+
+         int i;
+         for (i = 0; params[i] != NULL; i++)
+         {
+            free(params[i]);
+         }
+      }
+   }   
+
+   return 0;
+}
+
+void displayPrompt()
+{
+   printf("Enter a command: ");
+}
+
+void readCommand(char* command, char** params)
+{
+   char* lineOfInput = NULL;
+   size_t numBytes;
+ 
+   getline(&lineOfInput, &numBytes, stdin);
+
+   char* token;
+   token = strtok(lineOfInput, " \n");
+   
+   strcpy(command, token);
+
+   int counter = 0;
+
+   while (token != NULL)
+   {
+      params[counter] = (char*) malloc(strlen(token) + 1);
+      strcpy(params[counter], token);
+      token = strtok(NULL, " \n");
+      counter++;
+   }
+   params[counter] = NULL;
+}
+
+
